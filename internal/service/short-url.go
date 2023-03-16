1feed0eb84a7d76e9950c6b10f98d0c9a722cff6
mskoll <m.skoll@yandex.ru>
2023-03-16 11:50:28 +0300

    refactor

--- internal/service/short-url.go
+++ internal/service/short-url.go
@@ -4,32 +4,16 @@ import (
 	"github.com/spf13/viper"
 	"math/rand"
 	"strings"
-	"time"
 )
 
-const (
-	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	letterIdxBits = 6
-	letterIdxMask = 1<<letterIdxBits - 1
-	letterIdxMax  = 63 / letterIdxBits
-)
-
-var src = rand.NewSource(time.Now().UnixNano())
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func genShortUrl() string {
-	n := 5
+	n := 4
 	sb := strings.Builder{}
 	sb.Grow(n)
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			sb.WriteByte(letterBytes[idx])
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
+	for i := 0; i < n; i++ {
+		sb.WriteByte(charset[rand.Intn(len(charset))])
 	}
 	return sb.String()
 }
